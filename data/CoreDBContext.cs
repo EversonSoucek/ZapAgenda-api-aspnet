d42b99de748bf6740be40c2502529770a9b98e79
Everson <sousekjuniorr@outlook.com>
2025-01-20 00:51:19 -0300

    Adiciona modelo usuários

--- data/CoreDBContext.cs
+++ data/CoreDBContext.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ZapAgenda_api_aspnet.models;
 
@@ -6,8 +7,18 @@ namespace ZapAgenda_api_aspnet.data
     public class CoreDBContext : DbContext
     {
         public CoreDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
-        {}
+        { }
 
         public required DbSet<Empresa> Empresa { get; set; }
+
+        /*
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Usuario>()
+                .Property(u => u.UserName)
+                .HasColumnName("NomeDeUsuario");
+        }*/
     }
 }
---
+++ models/Usuario.cs
@@ -0,0 +1,17 @@
+/*using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace ZapAgenda_api_aspnet.models
+{
+    [Table("Usuario")]
+    public class Usuario : IdentityUser
+    {
+        [Key]
+        public int IdEmpresa { get; set; }
+        public DateTime UltimoLogin { get; set; }
+        public DateTime UltimaModificacao {get;set;}
+        public required string NomeInteiro {get;set;}
+        public bool Status { get; set; }
+    }
+}*/
